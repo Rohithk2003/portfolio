b0961694a6483990de7468ae351632e61db1c989
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2024-04-09 11:24:35 +0530

    updated main page and added new navbar

--- components/main.js
+++ components/main.js
@@ -1,62 +1,74 @@
 import Image from "next/image";
 // import profile from '../public/profile.jpg'
-import Typed from "typed.js"
+import {Inter} from "next/font/google";
+import Typed from "typed.js";
 import {useRef, useEffect} from "react";
 
+const inter = Inter({
+    subsets: ["latin"], weights: [300]
+})
 export default function Main() {
     const el = useRef(null);
-    useEffect(() => {
-        const typed = new Typed(el.current, {
-            strings: ["Hi , I am Rohith Krishnan <br> Based in India. <br> I'm a web-develop and ML enthusiast."],
-            typeSpeed: 50,
-            backSpeed: 50,
-        });
+    // useEffect(() => {
+    // 	const typed = new Typed(el.current, {
+    // 		strings: [
+    // 			"Hi , I am Rohith Krishnan <br> Based in India. <br> I'm a web-develop and ML enthusiast.",
+    // 		],
+    // 		typeSpeed: 50,
+    // 		backSpeed: 50,
+    // 	});
 
-        return () => {
-            typed.destroy();
-        };
-    }, []);
-    const text = "Rohith Krishnan";
-    return (
-        <div id={"home"}
-             className={" flex flex-wrap flex-row  bg-[#222222]   h-[100vh] pb-32 "}>
-            <div
-                className={" xl:w-1/2 w-full bg-[#222222] p-10 flex justify-center"}>
-                <div
-                    className={"text-white text-center animate-bounce justify-center md:pt-0 top-32 relative animate-infinite animate-duration-[2000ms] animate-ease-in-out  font-bold  md:text-9xl text-6xl  flex  flex-wrap items-center   z-[0]"}>
-                    Rohith
-                    Krishnan
-                </div>
-                {/*<div*/}
-                {/*    className={"md:w-[300px] h-[200px]  w-[200px] md:h-[300px] overflow-hidden absolute xl:bottom-10 bottom-auto xl:top-auto top-0 xl:right-auto right-[10%] xl:left-[30%] float-right rounded-full border-2 border-white object-cover"}>*/}
-                {/*    <Image className={"z-48 object-contain  "} src={"/portfolio/main.png"} alt={""}*/}
-                {/*           width={300}*/}
-                {/*           height={300}/></div>*/}
-            </div>
-            <div
-                className={" xl:w-1/2 w-full overflow-hidden h-full bg-[#222222] flex  justify-center align-middle items-center flex-row "}>
-                <div className={"flex justify-center flex-col h-max  md:pl-32 pl-6"}>
-                    <div
-                        className={"md:text-4xl text-2xl animate-fade-left animate-once animate-duration-[2000ms] animate-ease-in-out  min-h-36 font-bold based-data text-white "}>
-                        <span ref={el}/>
-                    </div>
-                    <div
-                        className={"text-2xl main-one-right animate-1 w-96  mt-10 font-bold based-data text-[#8f8f8f] relative "}>As
-                        a web developer,
-                        I
-                        code pixels into reality, turning dreams into clickable wonders! 💻<span>🚀</span>
-                    </div>
-                    <div
-                        className={"text-sm w-44  main-one-right animate-2 mt-10 font-bold based-data text-orange-300 tracking-wider"}>P:
-                        +91
-                        1234567890
-                    </div>
-                    <div
-                        className={"text-sm w-64  main-one-right animate-3 mt-1 font-bold based-data text-orange-300 tracking-wider"}>E:
-                        rohithk652@gmail.com
-                    </div>
+    // 	return () => {
+    // 		typed.destroy();
+    // 	};
+    // }, []);
+    // const text = "Rohith Krishnan";
+    return (<div
+        id={"home"}
+        className={` flex flex-wrap flex-row text-white      w-full bg-[#000000]   h-[100vh] ${inter.className}`}
+    >
+        <div className={"w-2/3 flex justify-center gap-4 items-center flex-col"}>
+            <div className={"flex justify-start w-1/2"}>
+                <div className={"w-24 h-24  rounded-full overflow-hidden object-bottom relative "}>
+                    <Image src={"/portfolio/main.png"}
+                           alt={"profile_photo w-full h-full"} style={{
+                        objectFit: "cover", objectPosition: "bottom",
+                    }} width={128}
+                           height={128}></Image>
                 </div>
             </div>
+            <div className={"text-xl text-start w-1/2 flex justify-start"}>
+                Hi, I am Rohith
+            </div>
+            <div className={"flex flex-col w-1/2 text-[#454545] mt-3"}>
+                    <span>
+                        A Software Developer who develops well-architected applications.
+                    </span>
+                <span>
+                        Passionate to develop them with engineering and design principles.
+                    </span>
+            </div>
+            <div className={" w-1/2 text-[#454545] "}>
+                Currently working on <span className={"text-white"}>being a better person and a developer</span>
+            </div>
+            <div className={"text-lg text-white text-start w-1/2 flex justify-start"}>
+                Top Skills
+            </div>
+            <ul className={"flex flex-row gap-4 text-[#585858] flex-wrap w-1/2 justify-start items-center"}>
+                <li>Next.js</li>
+                <li>Django</li>
+                <li>PostgresSQL</li>
+                <li>Machine Learning</li>
+                <li>Git</li>
+            </ul>
+            <div className={"text-lg text-white text-start w-1/2 flex justify-start"}>
+                Top Languages
+            </div>
+            <ul className={"flex flex-row gap-4 text-[#585858] flex-wrap w-1/2 justify-start items-center"}>
+                <li>Python</li>
+                <li>C++</li>
+                <li>Java</li>
+            </ul>
         </div>
-    )
+    < /div>);
 }
--- components/navbar.js
+++ components/navbar.js
@@ -1,107 +1,81 @@
 import Link from "next/link";
 import React, {useState} from "react";
+import {Inter} from "next/font/google";
 
+const inter = Inter({
+    subsets: ["latin"],
+    weight:['300']
+});
 
-const headings = [
-    "Home", "About", "Services", "Skills", "Contact",
-]
 export default function Navbar() {
-    const [HamStatus, setHamButtonStatus] = useState(false)
-    const [NavItemClicked, setNavItemStatus] = useState(false)
-    const [activeIdx, setActiveIdx] = useState(-1)
-    const [topBarVisible, setTopBarVisibility] = useState(false)
+    const [HamStatus, setHamButtonStatus] = useState(false);
+    const [NavItemClicked, setNavItemStatus] = useState(false);
+    const [activeIdx, setActiveIdx] = useState(-1);
+    const [topBarVisible, setTopBarVisibility] = useState(false);
 
     function handleHamButtonClick() {
-        setHamButtonStatus(!HamStatus)
-        setTopBarVisibility(!topBarVisible)
+        setHamButtonStatus(!HamStatus);
+        setTopBarVisibility(!topBarVisible);
     }
 
     function handleNavItemClicking() {
-        setNavItemStatus(!NavItemClicked)
+        setNavItemStatus(!NavItemClicked);
     }
 
     return (
         <nav>
-            <div className={"w-full  h-16 flex justify-between align-middle  z-[999] bg-[#222222] "}>
-                <div className={"w-1/3 mt-auto order-1 md:order-0 md:text-start text-start mb-auto ml-5 "}>
-                    <Link href={"/"}>
-                        <h4 className={"text-2xl font-bold"}>Portfo<span className={"text-[#f26c4f]"}>lio</span>.</h4>
-                    </Link>
-                </div>
-                <div className={"lg:flex hidden w-1/3 order-1  justify-start align-middle"}>
-                    <ul className={"flex  z-50  h-full text-center justify-start p-5 flex-column  gap-16 "}>
-                        {
-                            headings.map((heading, index) => {
-                                return (
-                                    <li
-                                        className={`mb-auto mt-auto text-center hover:text-[#f26c4f] transition-colors duration-300 ease-in-out ${activeIdx === index ? ("text-orange-400 border-b-2 border-amber-500") : ""}`}
-                                        onClick={() => {
-                                            setActiveIdx(index)
-                                            handleNavItemClicking()
-                                        }}
-                                        key={index}>
-                                        <Link href={`#${heading.toLowerCase()}`}>{heading}</Link>
-                                    </li>)
-                            })
-                        }
-                    </ul>
-                </div>
-                <div className={"flex lg:hidden  order-0 align-middle w-1/3 justify-start m-auto "}>
-                    <a className={" grid  ml-2 grid-rows-3w"} onClick={handleHamButtonClick}>
-                        <span
-                            className={`w-7 h-1 mb-1 bg-white transition-all duration-300 ease-in-out ${HamStatus ? (" rotate-45") : ""}`}></span>
-
-                        <span
-                            className={`w-7 h-1 mb-1 bg-white transition-all duration-300 ease-in-out ${HamStatus ? (" rotate-[-45deg] translate-y-[-8px]") : ""}`}></span>
-
-                        <span
-                            className={`w-7 h-1 mb-1 bg-white  ease-in-out ${HamStatus ? ("hidden") : ""}`}></span>
-                    </a>
-                </div>
-                <div
-                    className={"w-1/3 flex   order-2 lg:top-0 transition-all duration-300 ease-in-out items-center   justify-end mr-10 "}>
-                    <button className={"bg-[#f26c4f] text-black w-36 h-10 rounded"}><a
-                        href={"https://flowcv.com/resume/34p8i21ase"}>Download
-                        CV</a></button>
-                </div>
-
-            </div>
             <div
-                className={`w-full p-16 fixed transition inset-0 top-0 left-0  duration-800 ease-in-out  z-[999] justify-center items-center bg-black flex-col flex h-screen ${topBarVisible ? ('translate-x-0') : ('-translate-x-[100%]')}`}>
-                <div className={"flex  order-0 align-middle w-full absolute top-10  justify-start m-auto "}>
-                    <a className={" grid  ml-2 grid-rows-3w"} onClick={handleHamButtonClick}>
-                        <span
-                            className={`w-7 h-1 mb-1 bg-white transition-all duration-300 ease-in-out ${HamStatus ? (" rotate-45") : ""}`}></span>
+                className={` w-full  h-16 flex justify-end align-middle  z-[999] bg-[#000000] ${inter.className}`}
+            >
+                <ul
+                    className={
+                        "flex flex-row w-1/4 text-[#595959]  justify-center gap-10 items-center"
+                    }
+                >
+                    <li className="hover:text-white">Github</li>
+                    <li className="hover:text-white">Linkedin</li>
+                    <li className="hover:text-white">Projects</li>
+                </ul>
+                <div
+                    className={
+                        "md:hidden flex  order-0 align-middle w-full absolute top-10  justify-start m-auto "
+                    }
+                >
+                    <a
+                        className={" grid  ml-2 grid-rows-3w"}
+                        onClick={handleHamButtonClick}
+                    >
+						<span
+                            className={`w-7 h-1 mb-1 bg-white transition-all duration-300 ease-in-out ${HamStatus ? " rotate-45" : ""}`}
+                        ></span>
 
                         <span
-                            className={`w-7 h-1 mb-1 bg-white transition-all duration-300 ease-in-out ${HamStatus ? (" rotate-[-45deg] translate-y-[-8px]") : ""}`}></span>
+                            className={`w-7 h-1 mb-1 bg-white transition-all duration-300 ease-in-out ${HamStatus ? " rotate-[-45deg] translate-y-[-8px]" : ""}`}
+                        ></span>
 
                         <span
-                            className={`w-7 h-1 mb-1 bg-white  ease-in-out ${HamStatus ? ("hidden") : ""}`}></span>
+                            className={`w-7 h-1 mb-1 bg-white  ease-in-out ${HamStatus ? "hidden" : ""}`}
+                        ></span>
                     </a>
                 </div>
-                <div className={"w-1/3  "}>
-                    <h4 className={"text-2xl font-bold text-center mb-2"}>Portfo<span
-                        className={"text-[#f26c4f]"}>lio</span>.</h4>
-                </div>
-                <ul className={"flex w-full gap-2 flex-col"}>
-                    {
-                        headings.map((heading, index) => {
-                            return (
-                                <Link key={index} onClick={() => {
-                                    setTopBarVisibility(!topBarVisible);
-                                    setHamButtonStatus(!HamStatus);
-                                }} href={`#${heading.toLowerCase()}`}>
-                                    <li className={"w-full hover:text-white  flex justify-center bg-left-to-right items-center rounded hover:bg-[#f26c4f] transition-all duration-300 ease-in-out border-gray-500   h-16 border-2 "}>
-                                        {heading}
-                                    </li>
-                                </Link>
-                            )
-                        })
-                    }
+                <ul className={"flex md:hidden w-full gap-2 flex-col"}>
+
+                    <Link
+                        onClick={() => {
+                            setTopBarVisibility(!topBarVisible);
+                            setHamButtonStatus(!HamStatus);
+                        }}
+                     href={""}>
+                        <li
+                            className={
+                                "w-full hover:text-white  flex justify-center bg-left-to-right items-center rounded hover:bg-[#f26c4f] transition-all duration-300 ease-in-out border-gray-500   h-16 border-2 "
+                            }
+                        >
+                            hello
+                        </li>
+                    </Link>
                 </ul>
             </div>
         </nav>
-
-    )
+    );
 }
