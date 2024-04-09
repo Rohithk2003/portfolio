7c84a2815a0cac907122bdae717edefb75084d67
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2024-04-10 00:30:24 +0530

    update mobile design main page

--- components/main.js
+++ components/main.js
@@ -1,92 +1,104 @@
 import Image from "next/image";
 // import profile from '../public/profile.jpg'
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import Typed from "typed.js";
-import {useRef, useEffect} from "react";
+import { useRef, useEffect } from "react";
 
 const inter = Inter({
-    subsets: ["latin"],
-    weights: [300],
+	subsets: ["latin"],
+	weights: [300],
 });
 export default function Main() {
-    const el = useRef(null);
-    useEffect(() => {
-        const typed = new Typed(el.current, {
-            strings: [
-                "Hi , I am Rohith Krishnan", " Based in India.", "I'm a web-developer and ML enthusiast."
-            ],
-            typeSpeed: 50,
-            startDelay: 1500,
-            loop: true,
-            backSpeed: 50,
-        });
+	const el = useRef(null);
+	useEffect(() => {
+		const typed = new Typed(el.current, {
+			strings: [
+				"Hi , I am Rohith Krishnan",
+				" Based in India.",
+				"I'm a web-developer and ML enthusiast.",
+			],
+			typeSpeed: 50,
+			startDelay: 1500,
+			loop: true,
+			backSpeed: 50,
+		});
 
-        return () => {
-            typed.destroy();
-        };
-    }, []);
-    return (
-        <div
-            id={"home"}
-            className={`home flex flex-wrap flex-row text-white w-full bg-[#000000]   h-[100vh] ${inter.className}`}
-        >
-            <div className={"md:w-2/3 w-full flex justify-center gap-4 main-one items-center flex-col"}>
-                <div className={"flex md:justify-start justify-center md:w-1/2 w-full"}>
-                    <div className="flex justify-center items-center">
-                        <img src={"/images/profile.jpg"}
-                             alt="Profile Picture"
-                             className="w-32 h-32 rounded-full object-cover"/>
-                    </div>
-                </div>
+		return () => {
+			typed.destroy();
+		};
+	}, []);
+	return (
+		<div
+			id={"home"}
+			className={`home flex flex-wrap flex-row text-white w-full bg-[#000000]   h-[100vh] ${inter.className}`}
+		>
+			<div
+				className={
+					"md:w-2/3 w-full flex justify-center gap-4 main-one items-center flex-col"
+				}
+			>
+				<div className={"flex md:justify-start justify-center md:w-1/2 w-full"}>
+					<div className="flex justify-center items-center">
+						<img
+							src={"/images/profile.jpg"}
+							alt="Profile Picture"
+							className="w-32 h-32 rounded-full object-cover"
+						/>
+					</div>
+				</div>
 
-                <div className={"text-xl text-start w-1/2 flex justify-start"}>
-                    <span ref={el}/>
-                </div>
-                <div className={"flex flex-col w-1/2 text-[#454545] mt-3"}>
+				<div className={"text-xl text-start md:w-1/2 w-2/3 flex justify-start"}>
+					<span ref={el} />
+				</div>
+				<div className={"flex flex-col md:w-1/2 w-2/3 text-[#454545] mt-3"}>
 					<span>
 						A Software Developer who develops well-architected applications.
 					</span>
-                    <span>
+					<span>
 						Passionate to develop them with engineering and design principles.
 					</span>
-                </div>
-                <div className={" w-1/2 text-[#454545] "}>
-                    Currently working on{" "}
-                    <span className={"text-white"}>
+				</div>
+				<div className={" md:w-1/2 w-2/3 text-[#454545] "}>
+					Currently working on{" "}
+					<span className={"text-white"}>
 						&nbsp;being a better person and a developer
 					</span>
-                </div>
-                <div
-                    className={"text-lg text-white text-start w-1/2 flex justify-start"}
-                >
-                    Top Skills
-                </div>
-                <ul
-                    className={
-                        "flex flex-row gap-4 text-[#585858] flex-wrap w-1/2 justify-start items-center"
-                    }
-                >
-                    <li>Next.js</li>
-                    <li>Django</li>
-                    <li>PostgresSQL</li>
-                    <li>Machine Learning</li>
-                    <li>Git</li>
-                </ul>
-                <div
-                    className={"text-lg text-white text-start w-1/2 flex justify-start"}
-                >
-                    Top Languages
-                </div>
-                <ul
-                    className={
-                        "flex flex-row gap-4 text-[#585858] flex-wrap w-1/2 justify-start items-center"
-                    }
-                >
-                    <li>Python</li>
-                    <li>C++</li>
-                    <li>Java</li>
-                </ul>
-            </div>
-        </div>
-    );
+				</div>
+				<div
+					className={
+						"text-lg text-white text-start md:w-1/2 w-2/3 flex justify-start"
+					}
+				>
+					Top Skills
+				</div>
+				<ul
+					className={
+						"flex flex-row gap-4 text-[#585858] flex-wrap md:w-1/2 w-2/3 justify-start items-center"
+					}
+				>
+					<li>Next.js</li>
+					<li>Django</li>
+					<li>PostgresSQL</li>
+					<li>Machine Learning</li>
+					<li>Git</li>
+				</ul>
+				<div
+					className={
+						"text-lg text-white text-start md:w-1/2 w-2/3 flex justify-start"
+					}
+				>
+					Top Languages
+				</div>
+				<ul
+					className={
+						"flex flex-row gap-4 text-[#585858] flex-wrap md:w-1/2 w-2/3 justify-start items-center"
+					}
+				>
+					<li>Python</li>
+					<li>C++</li>
+					<li>Java</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
