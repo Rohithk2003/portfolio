84abc43024a1e67037517360ca464a44af6a81df
Rohith Krishnan <rohithk652@gmail.com>
2024-04-11 16:25:09 +0530

    Update contact form labels and placeholders

--- components/contact.js
+++ components/contact.js
@@ -110,7 +110,7 @@ export default function Contact() {
 								htmlFor="email"
 								className={`block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300`}
 							>
-								Your email
+								Email
 							</label>
 							<input
 								type="email"
@@ -118,7 +118,7 @@ export default function Contact() {
 								value={emailid}
 								onChange={(e) => setemailid(e.target.value)}
 								className={`shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-[#454545] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light`}
-								placeholder="name@flowbite.com"
+								placeholder="name@domain.com"
 								required
 							/>
 						</div>
@@ -140,7 +140,7 @@ export default function Contact() {
 								value={subject}
 								onChange={(e) => setSubject(e.target.value)}
 								className={`block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-[#454545] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light`}
-								placeholder="Let us know how we can help you"
+								placeholder="Subject..."
 								required
 							/>
 						</div>
@@ -154,7 +154,7 @@ export default function Contact() {
 								htmlFor="message"
 								className={`block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400`}
 							>
-								Your message
+								Explain your idea
 							</label>
 							<textarea
 								id="message"
@@ -162,7 +162,7 @@ export default function Contact() {
 								onChange={(e) => setMessage(e.target.value)}
 								rows="6"
 								className={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-[#454545] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500`}
-								placeholder="Leave a comment.."
+								placeholder="Explain your idea here..."
 							></textarea>
 						</div>
 						<button
