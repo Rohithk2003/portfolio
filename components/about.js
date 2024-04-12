ee16b99f44ea2e24710284b7e64f237eb00f2520
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2024-04-12 10:59:51 +0530

    Update scroll reveal animation in globals.css and about.js

--- components/about.js
+++ components/about.js
@@ -17,33 +17,38 @@ export default function About() {
 			id={"about"}
 			className={`${
 				inView ? "slide-in-left opacity-1" : "opacity-0"
-			} h-max md:p-20 p-10 text-justify pt-[-100px] lg:pt-36  bg-black flex flex-wrap justify-center ${
+			} h-max  md:p-20 p-10 text-justify pt-[-100px] lg:pt-36  flex flex-wrap justify-center ${
 				inter.className
 			}`}
 		>
 			`
 			<div
 				className={
-					"w-full lg:w-3/4 h-full   flex flex-col justify-center flex-wrap "
+					"w-full lg:w-3/4 h-full   flex flex-col justify-center flex-wrap scroll-reveal"
 				}
 			>
 				<div className={"w-full mb-2"}>
-					<h4 className={"text-[#454545] "}>About me </h4>
-					<div className={"w-44  border-b border-b-white text-[#ef6c50]"}></div>
+					<h4 className={"text-white "}>About me </h4>
+					<div className={"w-44  border-b border-b-white "}></div>
 				</div>
-				<div className={"w-full text-md text-white"}>
+				<div className={"w-full text-md "}>
 					<div className={"w-full mb-4 font-bold text-lg lg:text-2xl"}>
-						You {`can't`} use up creativity. The more you use, the more you have
-						in your significant mind.
+						<span>
+							You {`can't`} use up creativity. The more you use, the more you
+							have in your significant mind.
+						</span>
 					</div>
-					<div className={"md:text-xl text-lg text-[#454545]  w-full"}>
-						Dedicated student with a strong passion for coding, particularly in
-						web development and machine learning. Skilled at using the latest
-						technologies to create innovative software. Committed to continuous
-						learning and professional growth, staying updated with the latest
-						industry trends and best practices. Driven by curiosity and a desire
-						to push the boundaries of what&apos;s possible through code.
-					</div>
+					<section className={"md:text-xl text-lg   w-full"}>
+						<span>
+							Dedicated student with a strong passion for coding, particularly
+							in web development and machine learning. Skilled at using the
+							latest technologies to create innovative software. Committed to
+							continuous learning and professional growth, staying updated with
+							the latest industry trends and best practices. Driven by curiosity
+							and a desire to push the boundaries of what&apos;s possible
+							through code.
+						</span>
+					</section>
 				</div>
 			</div>
 		</div>
