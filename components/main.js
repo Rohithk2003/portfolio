4db44eae373765cba3bd3111c6f524942a429907
Rohith Krishnan <rohithk652@gmail.com>
2024-04-14 20:17:40 +0530

    changed font size and height for main page

--- components/main.js
+++ components/main.js
@@ -30,15 +30,15 @@ export default function Main() {
 	return (
 		<div
 			id={"home"}
-			className={`home flex bg-black flex-wrap  flex-col w-full h-dvh gap-5 md:gap-17!  pr-0  md:pl-44 sm:pl-14 pl-5 md:p-20 sm:p-14 p-5`}
+			className={`home flex bg-black flex-wrap  flex-col w-full h-dvh gap-3 md:gap-15!  pr-0  md:pl-44 sm:pl-14 pl-5 md:p-20 sm:p-14 p-5`}
 		>
-			<div className="md:h-1 h-3 w-full text-gray-300 sf-mono-text ">
+			<div className="md:h-3 h-6 w-full text-gray-300 sf-mono-text ">
 				Hi, my name is
 			</div>
-			<div className="home-name text-white calibre-font md:text-8xl sm:text-6xl text-4xl font-bold">
+			<div className="home-name text-white calibre-font md:text-7xl sm:text-6xl xs:text-5xl text-2xl font-bold">
 				Rohith Krishnan
 			</div>
-			<div className="home-sub-text calibre-font md:text-7xl sm:text-5xl xs:text-4xl text-xl font-bold text-gray-300">
+			<div className="home-sub-text calibre-font md:text-7xl sm:text-6xl xs:text-5xl text-xl font-bold text-gray-300">
 				I build things for the web
 			</div>
 			<div className="home-text md:w-1/2 sm:w-2/3 w-full text-gray-400 font-[400]  calibre-font md:text-[20px] text-[17.5px] pr-5">
