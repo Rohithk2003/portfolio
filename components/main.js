cc2e6c403e6daef71cdaa0ccf96f0724246d374c
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2024-04-14 22:15:53 +0530

    adding space between button and main div

--- components/main.js
+++ components/main.js
@@ -30,7 +30,7 @@ export default function Main() {
 	return (
 		<div
 			id={"home"}
-			className={`home pop-up md:animate-9 animate-2 flex bg-black flex-wrap  flex-col w-full h-dvh xs:gap-3 gap-6 md:gap-15! lg:gap-12  md:pl-44 sm:pl-14 pl-5 md:p-20 sm:p-14 p-5 xs:pt-0 pt-32  pr-2 `}
+			className={`home pop-up md:animate-9 animate-2 flex bg-black flex-wrap  flex-col w-full h-dvh xs:gap-3 gap-6 md:gap-15! lg:gap-12  md:pl-44 sm:pl-14 pl-5 md:p-20 sm:p-14 p-5 xs:pt-0 pt-20  pr-2 `}
 		>
 			<div className="md:h-3 h-6 w-full text-gray-300 sf-mono-text ">
 				Hi, my name is
@@ -46,7 +46,7 @@ export default function Main() {
 				designing) exceptional digital experiences. Currently, I&apos;m a
 				student at Amrita Vishwa Vidyapeetham based in India.
 			</div>
-			<button className="bg-white sf-mono-text text-black rounded-md sm:w-44 w-32 text-md h-12 btn-style5">
+			<button className="bg-white sf-mono-text sm:mt-0 mt-24 text-black rounded-md sm:w-44 w-32 text-md h-12 btn-style5">
 				Say Hello!
 			</button>
 		</div>
