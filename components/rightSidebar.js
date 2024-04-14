510cffc7a6a1787ac3b6c12711a64abbb8ead949
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2024-04-14 23:42:55 +0530

    updated animation delay of right sidebar

--- components/rightSidebar.js
+++ components/rightSidebar.js
@@ -4,12 +4,12 @@ export default function RightSidebar() {
 	return (
 		<div className="w-24  h-[100vh] top-0 right-0 fixed bg-black text-[#454545] flex flex-col justify-end items-center">
 			<div className="w-full h-3/4 btext-black flex flex-col  justify-end items-start  ">
-				<div className="flex flex-col h-52 w-24 justify-center items-center  pb-10 pop-up animate-11">
+				<div className="flex flex-col h-52 w-24 justify-center items-center  pb-10 pop-up animate-8">
 					<Link href={"mailto:rohithk652@gmail.com"}>
 						<div className="rotated-text ">rohithk652@gmail.com</div>
 					</Link>
 				</div>
-				<div className="h-32 bg-black w-full flex justify-center items-center pop-up animate-11">
+				<div className="h-32 bg-black w-full flex justify-center items-center pop-up animate-8">
 					<div className="h-32 bg-[#454545] w-px "></div>
 				</div>
 			</div>
