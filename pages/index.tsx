4ea872dc787c2cef283c0de780909da9d6b01f3e
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2024-04-15 22:24:59 +0530

    added experience

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,46 +11,44 @@ import Head from "next/head";
 import LeftSidebar from "@/components/leftSIdebar";
 import RightSidebar from "@/components/rightSidebar";
 
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
+import Experience from "@/components/experience";
 
 export default function Home() {
-	const [title, setTitle] = useState("Hello there fellow Coder!");
-	useEffect(() => {
-		setTimeout(() => {
-			if (title === "Hello there fellow Coder!")
-				setTitle("Welcome to my Portfolio!");
-			else setTitle("Hello There Fellow Coder");
-		}, 2000);
-	});
-	return (
-		<>
-			<Head>
-				<title>{title}</title>
-			</Head>
-			<Navbar />
-			{/* <Main /> */}
-			{/* <About /> */}
-			{/* <Project /> */}
-			{/* <Services /> */}
-			{/* <Skills/> */}
-			{/* <ContactDetails /> */}
-			{/* <Footer /> */}
-			<div className={"flex "}>
-				<div className={"w-[5%] md:block hidden"}>
-					<LeftSidebar />
-				</div>
-				<div className={"md:w-[90%] h-[500vh] w-full text-white bg-black"}>
-					<Main />
-					<About />
-				</div>
-				<div className={"w-[5%] md:block hidden"}>
-					<RightSidebar />
-				</div>
-			</div>
-			<Script
-				src="https://kit.fontawesome.com/93a493583b.js"
-				crossOrigin="anonymous"
-			></Script>
-		</>
-	);
+    const [title, setTitle] = useState("Web Developer | Rohith Krishnan");
+    useEffect(() => {
+       
+    });
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <Navbar/>
+            {/* <Main /> */}
+            {/* <About /> */}
+            {/* <Project /> */}
+            {/* <Services /> */}
+            {/* <Skills/> */}
+            {/* <ContactDetails /> */}
+            {/* <Footer /> */}
+            <div className={"flex "}>
+                <div className={"w-[5%] md:block hidden"}>
+                    <LeftSidebar/>
+                </div>
+                <div className={"md:w-[90%] h-[500vh] w-full text-white bg-black"}>
+                    <Main/>
+                    <About/>
+                    <Experience/>
+                </div>
+                <div className={"w-[5%] md:block hidden"}>
+                    <RightSidebar/>
+                </div>
+            </div>
+            <Script
+                src="https://kit.fontawesome.com/93a493583b.js"
+                crossOrigin="anonymous"
+            ></Script>
+        </>
+    );
 }
